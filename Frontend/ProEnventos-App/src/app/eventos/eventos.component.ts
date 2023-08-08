52b2f453b4edb0fe7e297ec12aaf235af58616dd
pedrofranq96 <pedrofranq96@gmail.com>
2023-08-08 02:44:03 -0300

    frontend - interpolação, diretivas e binding

--- Frontend/ProEnventos-App/src/app/eventos/eventos.component.ts
+++ Frontend/ProEnventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public filtredEvents: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.filtredEvents = this.listFilter ? this.eventFilter(this.listFilter) : this.eventos;
+  }
+
+  eventFilter(filter: string): any {
+    filter = filter.toLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filter) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filter) !== -1
+
+    )
+  }
 
   constructor(private http:HttpClient) { }
 
@@ -16,9 +39,16 @@ export class EventosComponent implements OnInit {
     this.getEventos()
   }
 
+  alterImg(){
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
     this.http.get(`https://localhost:5001/api/eventos`).subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.filtredEvents = this.eventos
+      },
       error => console.log(error)
     )
   }
